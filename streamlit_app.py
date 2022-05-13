acfad77a42b9f529b70040e6229a6c2631efb08c
snow2706 <105484811+snow2706@users.noreply.github.com>
2022-05-13 14:03:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
+import pandas
+
 streamlit.title('Hello Snowflake')
 streamlit.title('Learing the new things')
 streamlit.header('Dataware Housing')
 streamlit.text('Good data management')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
