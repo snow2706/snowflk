5035807e1e038e619c2e797f0efdac781b6a653e
snow2706 <105484811+snow2706@users.noreply.github.com>
2022-05-21 14:46:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,4 @@ streamlit.dataframe(fruit_to_show)
 # NEW Section for request
 
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon");
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
