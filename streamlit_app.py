74fcb309928791318159bc6f9c07dc81748e9bb8
snow2706 <105484811+snow2706@users.noreply.github.com>
2022-05-21 14:59:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,10 @@ streamlit.dataframe(fruit_to_show)
 
 #streamlit.text(fruityvice_response.json())
 streamlit.header('Fruit vice Advice')
-fruit_choice = streamlit.text_input('What fruit you like','KIWI')
+fruit_choice = streamlit.text_input('What fruit you like','Kiwi')
 streamlit.write('User enter Choice',fruit_choice)
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/",+fruit_choice);
+
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/",+fruit_choice)
 
 # take json response 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
