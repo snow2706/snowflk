97430870d0fb9fc1e657cf94ad1e681939bf823f
snow2706 <105484811+snow2706@users.noreply.github.com>
2022-05-21 17:03:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+
 
 streamlit.title('Hello Snowflake')
 streamlit.title('Learing the new things')
@@ -27,10 +27,11 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choic
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur=my_cnx.cusrsor()
-my_cur.execute("SELECT CURRENT_USER(),CURRNET_ACCOUNT(),CURRNET_REGION()")
-my_data=my_cur.fetchone()
-streamlit.text("Hello from snowflake")
-streamlit.text(my_data)
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
